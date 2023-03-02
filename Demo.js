9c38b80b26e7fe0513e288318e86457730f26c7d
Dimitri2105 <karanthakur577@gmail.com>
2023-03-02 11:25:10 +0530

    Array, Spread Operator , Rest Operator

--- Demo.js
+++ Demo.js
@@ -1,15 +1,41 @@
-const productOfNumber = (a,b) =>{
-    return (a*b);
-}
+// const productOfNumber = (a,b) =>{
+//     return (a*b);
+// }
 
-console.log(productOfNumber(7,8))
+// console.log(productOfNumber(7,8))
 
-let student = {
-    name : "karan",
-    age : 23,
-    course:"CSE",
-    getInfo(){
-        console.log(`Student Name is ${this.name}`)
-    }
-}
-student.getInfo()
+// let student = {
+//     name : "karan",
+//     age : 23,
+//     course:"CSE",
+//     getInfo(){
+//         console.log(`Student Name is ${this.name}`)
+//     }
+// }
+// student.getInfo()
+
+
+// const fruits =['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'] 
+// const newArray = fruits.map( (fruit) =>{
+//         if (fruit === ' '){
+//             return 'empty string'
+//         }
+//         else{
+//             return fruit
+//         }
+// })
+// console.log(newArray)
+
+
+// Spread Operator
+
+// const newFruits = [...fruits,"tomato"]
+// console.log(newFruits)
+
+// Rest Opertor
+
+// const myFunc  =(...n) =>{
+//     return n
+// }
+// console.log(myFunc(1,2,3,4,5,6))
+
