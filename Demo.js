77496980ba94abeb0a5bd26107d19f300aa11add
Dimitri2105 <karanthakur577@gmail.com>
2023-03-02 11:58:56 +0530

    Destucturing

--- Demo.js
+++ Demo.js
@@ -39,3 +39,29 @@
 // }
 // console.log(myFunc(1,2,3,4,5,6))
 
+// Destructuring -----------------------------------------------
+
+// Array Destructuring
+
+const fruits =['apple', 'oranges' , 'tomato ', 'mango', 'pineapple ' , 'lemon'] 
+
+const [fruit1,fruit2,,fruit3] = fruits
+console.log(fruit1,fruit2,fruit3)
+
+// Object Destructuring
+
+const student = {
+    name : "karan",
+    age : 23,
+    course:"CSE",
+    getInfo(){
+        console.log(`Student Name is ${this.name}`)
+    }
+}
+
+const {name,course} = student
+console.log(name,course)
+
+
+
+
