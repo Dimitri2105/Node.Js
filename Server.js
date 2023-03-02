e2c76863eaf3cf5b257e5d35477a927d4771158b
Dimitri2105 <karanthakur577@gmail.com>
2023-03-02 14:09:33 +0530

    Request / Response

--- Server.js
+++ Server.js
@@ -4,8 +4,34 @@ const { request } = require('https')
 
 const server = http.createServer( (request,response) =>{
 
-    console.log("Karan Thakur")
+    
+    if(request.url === '/'){
+    response.setHeader('Content-Type' , 'Test/html')
+    response.write('<html>')
+    response.write('<head> My Page </head')
+    response.write('<body><h1>Welcome home</h1></body>')
+    response.write('</html>')
+    return response.end()
+
+    }else if (request.url === '/about'){
+    response.setHeader('Content-Type' , 'Test/html')
+    response.write('<html>')
+    response.write('<head> My Page </head')
+    response.write('<body><h1> Welcome to About Us page</h1></body>')
+    response.write('</html>')
+    return response.end()
+    }else{
+    response.setHeader('Content-Type' , 'Test/html')
+    response.write('<html>')
+    response.write('<head> My Page </head')
+    response.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    response.write('</html>')
+    return response.end()
+
+    }
+    
+
 
 })
 
-server.listen(4000)
+server.listen(3000)
