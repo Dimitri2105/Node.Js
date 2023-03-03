4bd86639f2c57e1c41ecf7ba4e7945d208a46bb6
Dimitri2105 <karanthakur577@gmail.com>
2023-03-03 10:15:15 +0530

    Writing and Reading From File

--- Server.js
+++ Server.js
@@ -1,37 +1,41 @@
-const http = require('http')
-const { request } = require('https')
+const http = require("http");
+const fs = require("fs");
 
+const server = http.createServer((request, response) => {
+  const url = request.url;
+  const method = request.method;
+  if (url === "/") {
+    response.write("<html>");
+    response.write("<head> Enter Message </head");
+    response.write(
+      '<body><form action="/message" method = "POST"><input type = "text" name="message"><button>Submit</button><</form></body>'
+    );
+    response.write("</html>");
+    return response.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return request.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
 
-const server = http.createServer( (request,response) =>{
+      fs.writeFile("message.txt", message, () => {
+        response.statusCode = 302;
+        response.setHeader("Location", "/");
+        return response.end();
+      });
+    });
+  }
 
-    
-    if(request.url === '/'){
-    response.setHeader('Content-Type' , 'Test/html')
-    response.write('<html>')
-    response.write('<head> My Page </head')
-    response.write('<body><h1>Welcome home</h1></body>')
-    response.write('</html>')
-    return response.end()
+  response.setHeader("Content-Type", "text/html");
+  response.write("<html>");
+  response.write("<head> My Page </head");
+  response.write("<body><h1> Welcome to About Us page</h1></body>");
+  response.write("</html>");
+  response.end();
+});
 
-    }else if (request.url === '/about'){
-    response.setHeader('Content-Type' , 'Test/html')
-    response.write('<html>')
-    response.write('<head> My Page </head')
-    response.write('<body><h1> Welcome to About Us page</h1></body>')
-    response.write('</html>')
-    return response.end()
-    }else{
-    response.setHeader('Content-Type' , 'Test/html')
-    response.write('<html>')
-    response.write('<head> My Page </head')
-    response.write('<body><h1>Welcome to my Node Js project</h1></body>')
-    response.write('</html>')
-    return response.end()
-
-    }
-    
-
-
-})
-
-server.listen(3000)
+server.listen(3000);
