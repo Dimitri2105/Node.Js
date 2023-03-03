8f00f39ba3e107c8b53422884bdb20b52994b4f9
Dimitri2105 <karanthakur577@gmail.com>
2023-03-03 10:54:12 +0530

    Writing, Reading and Displaying on screen From File

--- Server.js
+++ Server.js
@@ -1,19 +1,27 @@
 const http = require("http");
 const fs = require("fs");
+const { DEFAULT_ENCODING } = require("crypto");
 
 const server = http.createServer((request, response) => {
   const url = request.url;
   const method = request.method;
   if (url === "/") {
+    fs.readFile('message.txt',(err,data) =>{
     response.write("<html>");
+    response.write(`<body>${data}</body>`)
+    response.write("<br>")
     response.write("<head> Enter Message </head");
     response.write(
       '<body><form action="/message" method = "POST"><input type = "text" name="message"><button>Submit</button><</form></body>'
     );
     response.write("</html>");
     return response.end();
+
+    })
+    
   }
-  if (url === "/message" && method === "POST") {
+
+  else if (url === "/message" && method === "POST") {
     const body = [];
     request.on("data", (chunk) => {
       body.push(chunk);
@@ -29,13 +37,17 @@ const server = http.createServer((request, response) => {
       });
     });
   }
-
+  else{
   response.setHeader("Content-Type", "text/html");
   response.write("<html>");
   response.write("<head> My Page </head");
   response.write("<body><h1> Welcome to About Us page</h1></body>");
   response.write("</html>");
   response.end();
+
+  }
+
+  
 });
 
 server.listen(3000);
